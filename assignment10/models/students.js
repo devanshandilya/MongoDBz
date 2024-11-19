f58fbf60696a1a7ec28ebc8ca23b1ec5cb8d943c
Devansh Shandilya <122812994+devanshandilya@users.noreply.github.com>
2024-11-19 14:22:04 +0530

    Update students.js

--- assignment10/models/students.js
+++ assignment10/models/students.js
@@ -1,6 +1,10 @@
 const mongoose=require("mongoose")
 
 const studentSchema=new mongoose.Schema({
+ name:{type: String, required:true},
+ age:{type:Number, reqired:true},
+ grade:{type:String, required:true},
+ courses:[{type:mongoose.Schema.Types.ObjectId, ref: "Courses}]
  
 })
 module.exports=mongoose.model("Student", studentSchema)
